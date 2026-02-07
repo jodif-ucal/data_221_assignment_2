6c368ee511e587f0111eb45a09c6c20472a37b1c
jodif_ucal <joseph.odihi@ucalgary.ca>
2026-02-07 13:05:48 -0700

    Add commments to explain code for question 3

--- Data_221_Assignment_2_Question_3.py
+++ Data_221_Assignment_2_Question_3.py
@@ -11,6 +11,7 @@ with open("csv_and_txt_files/sample-file.txt", "r") as file:
     # This is possible through the file.readlines() method
     lines_from_text_file = file.readlines()
 
+#This is to keep track of the original line number the line was found on
 for i in range(len(lines_from_text_file)):
     lines_from_text_file[i] = {
         "Original line": lines_from_text_file[i],
@@ -29,26 +30,46 @@ for line in tuple(lines_from_text_file):
 
 for index in range(len(lines_from_text_file)):
     line = lines_from_text_file[index]
+    #This is to ensure edited_lines doesn't just have an empty string inside if not touched after the next block
     edited_line = lines_from_text_file[index]["Original line"]
 
+    #Removing punctuation marks
     for punctuation in PUNCTUATION:
         if punctuation in line["Original line"]:
             edited_line = edited_line.replace(punctuation, "")
 
+    #Changing the line to lower case, removing white space and removing newline characters for
+    #future comparisons
     edited_line = edited_line.rstrip(NEWLINE_CHARACTER).lower().replace(" ", "")
 
+    #Adding the edited line to the dictionary
     line["Edited line"] = edited_line
     formatted_lines.append(line)
 
+#Finding the near identical pairs of lines
 for dictionary1 in formatted_lines:
     for dictionary2 in formatted_lines:
+        '''
+        The 'is' keyword compares the dictionaries by reference.
+        The == operator will only check if the dictionary contain identical things.
+        This is so then we don't accidentally save the same dictionary as a near identical pair,
+        as well as skipping redundant comparisons (we obviously don't want a dictionary to compare itself).
+        Using the 'is' keyword is quicker to do than finding both of their indexes, due to the nature 
+        of this nested loop
+        '''
         if dictionary1 is dictionary2:
             continue
 
         if dictionary1["Edited line"] == dictionary2["Edited line"]:
+            '''
+            This prevents the same pair we identified earlier from being put in the pairs list again.
+            This is also a good demonstration of the difference between comparing using the keyword 'is'
+            to simply comparing data structures using == or 'in'.
+            '''
             if (dictionary2, dictionary1) not in nearly_identical_pairs:
                 nearly_identical_pairs.append((dictionary1, dictionary2))
 
+#Printing the near identical pairs
 for near_identical_pair in nearly_identical_pairs:
     print(
         f'''
